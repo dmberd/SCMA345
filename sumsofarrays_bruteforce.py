81e287ac8462dcff9383ffc49bea2e7552e29599
dmberd <berdinsky@gmail.com>
2021-01-21 18:01:50 +0700

    Update sumsofarrays_bruteforce.py

--- sumsofarrays_bruteforce.py
+++ sumsofarrays_bruteforce.py
@@ -1,3 +1,4 @@
+# quadratic time algorithm 
 a=[1,2,3,4,5,6,7,8,9,10,6,8,9,4]
 
 m=int(len(a)/2)
@@ -7,9 +8,8 @@ for i in range(0,m+1):
     s = 0
     for j in range(0,m):
        s=s + a[i+j]    
-       t= t+1  # running time 
-    r = r + s   
-    t = t+1   # updating running time 
-print('The answer is:',r)
+       t=t+1  #updating the number of elementary operations 
+    print ('The sum of elements from',i,'to',i+m-1,'is:',s)    
+
 print('The size of input is:',len(a))
 print('The total number of elementary operations is:',t)
